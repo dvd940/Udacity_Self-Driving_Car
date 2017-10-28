5d025ca3ae8a2caef53b5433cbb3f487df7469f6
dave <dunnedave+coursera@gmail.com>
2017-10-28 11:58:25 -0700

    Change line smoothing from mean to median to help with outliers

--- Project4/project_modules/find_lanes.py
+++ Project4/project_modules/find_lanes.py
@@ -95,8 +95,9 @@ def curvature_measurement(input_image, left_fit, right_fit, left_lane_inds, righ
     left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
-    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
-    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)  
+    left_fit_cr = np.polyfit(np.array(lefty,dtype=np.float32)*ym_per_pix, np.array(leftx,dtype=np.float32)*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(np.array(righty,dtype=np.float32)*ym_per_pix, np.array(rightx,dtype=np.float32)*xm_per_pix, 2)
+    
     # Calculate the new radii of curvature
     left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
     right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
@@ -126,7 +127,7 @@ def line_smoothing(lane, fitx, curvature):
     if len(lane.recent_xfitted) > smoothing_factor:
         lane.recent_xfitted.pop(0)
 
-    lane.bestx = np.mean(lane.recent_xfitted,axis=0)
+    lane.bestx = np.median(lane.recent_xfitted,axis=0)
     
     return lane.bestx if lane.bestx is not None else fitx
 
@@ -140,6 +141,7 @@ class Line():
         self.bestx = None
         #polynomial coefficients for the most recent fit
         self.current_fit = [np.array([False])] 
-
-        # Diagnostics
+        # Keep a list of the curvature for display
         self.curve_list = []
+
+
